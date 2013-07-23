b95793a127acfdc02814f90b2f29386deba36a8e
tjmehta <tejesh.mehta@gmail.com>
2013-07-23 13:29:26 -0700

    improved postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,43 +1,50 @@
-// // We have to make sure some client-side dependencies
-// // actually exist in node_modules.
+// We have to make sure some client-side dependencies
+// actually exist in node_modules.
 
-// var npm = require("npm");
+var npm = require("npm");
+var fs = require("fs");
+var path = require("path");
 
 
-// var root = __dirname + '/..',
-//   pkg = require('../package.json');
+var root = __dirname + '/..',
+  pkg = require('../package.json');
 
-// var dependencies = [
-//   'underscore',
-//   'backbone',
-//   'async',
-//   'handlebars'
-// ];
+var dependencies = [
+  'underscore',
+  'backbone',
+  'async',
+  'handlebars'
+];
 
-// var packages = dependencies.map(function(dep) {
-//   var pkgDep = pkg.dependencies[dep];
+dependencies.filter(function (dep) {
+  var p = path.join(__dirname, 'node_modules', dep);
+  return !fs.existsSync(p);
+});
 
-//   if (~pkgDep.indexOf('://')) {
-//     // If it has a protocol, assume it's a link to a repo.
-//     return pkgDep;
-//   } else {
-//     return dep + '@' + pkgDep;
-//   }
-// });
+var packages = dependencies.map(function(dep) {
+  var pkgDep = pkg.dependencies[dep];
 
-// npm.load({
-//   'cwd': root
-// }, function(err) {
-//   if (err) return handleError(err);
-//   npm.commands.install(packages, function(err, message) {
-//     if (err) return handleError(err);
-//     console.log("NPM POSTINALL: %s", message);
-//   });
-//   npm.on("log", function(message) {
-//     console.log("NPM POSTINALL: %s", message);
-//   });
-// });
+  if (~pkgDep.indexOf('://')) {
+    // If it has a protocol, assume it's a link to a repo.
+    return pkgDep;
+  } else {
+    return dep + '@' + pkgDep;
+  }
+});
 
-// function handleError(err) {
-//   console.error(err.stack || err.message);
-// }
+npm.load({
+  'cwd': root
+}, function(err) {
+  if (err) return handleError(err);
+  npm.commands.install(packages, function(err, message) {
+    if (err) return handleError(err);
+    console.log("NPM POSTINALL: %s", message);
+  });
+  npm.on("log", function(message) {
+    console.log("NPM POSTINALL: %s", message);
+  });
+});
+
+function handleError(err) {
+  console.error(err.stack || err.message);
+}
