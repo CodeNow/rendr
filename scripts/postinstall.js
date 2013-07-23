1ada3f4eb81d91e510ce5c828bbfdfe286342843
tjmehta <tejesh.mehta@gmail.com>
2013-07-23 13:23:36 -0700

    trying to remove postinstall bc it takes too long..

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,43 +1,43 @@
-// We have to make sure some client-side dependencies
-// actually exist in node_modules.
+// // We have to make sure some client-side dependencies
+// // actually exist in node_modules.
 
-var npm = require("npm");
+// var npm = require("npm");
 
 
-var root = __dirname + '/..',
-  pkg = require('../package.json');
+// var root = __dirname + '/..',
+//   pkg = require('../package.json');
 
-var dependencies = [
-  'underscore',
-  'backbone',
-  'async',
-  'handlebars'
-];
+// var dependencies = [
+//   'underscore',
+//   'backbone',
+//   'async',
+//   'handlebars'
+// ];
 
-var packages = dependencies.map(function(dep) {
-  var pkgDep = pkg.dependencies[dep];
+// var packages = dependencies.map(function(dep) {
+//   var pkgDep = pkg.dependencies[dep];
 
-  if (~pkgDep.indexOf('://')) {
-    // If it has a protocol, assume it's a link to a repo.
-    return pkgDep;
-  } else {
-    return dep + '@' + pkgDep;
-  }
-});
+//   if (~pkgDep.indexOf('://')) {
+//     // If it has a protocol, assume it's a link to a repo.
+//     return pkgDep;
+//   } else {
+//     return dep + '@' + pkgDep;
+//   }
+// });
 
-npm.load({
-  'cwd': root
-}, function(err) {
-  if (err) return handleError(err);
-  npm.commands.install(packages, function(err, message) {
-    if (err) return handleError(err);
-    console.log("NPM POSTINALL: %s", message);
-  });
-  npm.on("log", function(message) {
-    console.log("NPM POSTINALL: %s", message);
-  });
-});
+// npm.load({
+//   'cwd': root
+// }, function(err) {
+//   if (err) return handleError(err);
+//   npm.commands.install(packages, function(err, message) {
+//     if (err) return handleError(err);
+//     console.log("NPM POSTINALL: %s", message);
+//   });
+//   npm.on("log", function(message) {
+//     console.log("NPM POSTINALL: %s", message);
+//   });
+// });
 
-function handleError(err) {
-  console.error(err.stack || err.message);
-}
+// function handleError(err) {
+//   console.error(err.stack || err.message);
+// }
